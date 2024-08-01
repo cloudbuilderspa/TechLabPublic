8465c7b38b58c235e02b9cf6ed4378b00b9861cc
Boris Zamora <boris@cloudbuilder.cl>
2024-08-01 12:35:05 -0400

    fix tf state bucket for rds

--- infraestructure/dev/rds/backend.tf
+++ infraestructure/dev/rds/backend.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "techlab-infraestructure-dev"
+    bucket         = "techlab-infraestructure-dev-rds"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
     # Replace this with your DynamoDB table name!
-    dynamodb_table = "ccp-infraestructure-dev"
+    dynamodb_table = "techlab-infraestructure-dev-rds"
     encrypt        = true
   }
 }
